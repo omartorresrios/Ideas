a59bf8623e756ecd8f52082ab9faaf8ab49a89dc
Omar Torres <omar.torres@fueled.com>
2023-08-31 22:14:38 -0500

    Initial Commit

---
+++ Ideas/IdeasApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdeasApp.swift
+//  Ideas
+//
+//  Created by Omar Torres on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct IdeasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
